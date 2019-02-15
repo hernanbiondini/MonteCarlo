eb8c535911563c6294d399ac46fbd7eabfb241d0
Hernan <hernanbiondini@gmail.com>
2019-02-15 14:01:03 -0300

    Pruebas de Integrales 6

--- src/integralmontecarlo/applet/MonteCarloApplet.java
+++ src/integralmontecarlo/applet/MonteCarloApplet.java
@@ -8,18 +8,23 @@ import java.applet.Applet;
 import java.text.DecimalFormat;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MonteCarloApplet extends Applet implements Runnable, ActionListener {
 
     private Label count;
     private Label piApprox;
+    private Label inicioLabel;
     private Button startButton;
     private Button stopButton;
     private MonteCarloCanvas drawArea;
     private Panel messageArea;
+    private Panel messageArea2;
     private int numPoints = 0;
     private int pointsInCircle = 0;
     private DecimalFormat df = new DecimalFormat("0.000000000000");
+    private String horaStr = "";
 
     // Si se establece en verdadero, reinicia el cálculo.    
     private boolean restart_p = false;
@@ -39,28 +44,54 @@ public class MonteCarloApplet extends Applet implements Runnable, ActionListener
     // Coloca el área del mensaje en la parte inferior.
     private Panel makeMessageArea() {
         messageArea = new Panel();
+        messageArea2 = new Panel();
         GridBagLayout gb = new GridBagLayout();
+        
+        GridBagLayout gb2 = new GridBagLayout();
+        
         GridBagConstraints gbc = new GridBagConstraints();
+        
+        GridBagConstraints gbc2 = new GridBagConstraints();
+        
         messageArea.setLayout(gb);
+        messageArea2.setLayout(gb2);
+        
         Label countLabel = new Label("N", Label.CENTER);
         Label piLabel = new Label("pi (aprox)", Label.CENTER);
         count = new Label("00000000", Label.CENTER);
         piApprox = new Label("0.000000000000", Label.CENTER);
+        inicioLabel = new Label("00:00:00", Label.CENTER);
+       
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
         gbc.gridwidth = GridBagConstraints.RELATIVE;
         gbc.fill = GridBagConstraints.HORIZONTAL;
+        
+        gbc2.weightx = 1.0;
+        gbc2.weighty = 1.0;
+        gbc2.gridwidth = GridBagConstraints.RELATIVE;
+        gbc2.fill = GridBagConstraints.HORIZONTAL;
+        
         gb.setConstraints(count, gbc);
+        
         messageArea.add(count);
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        
+        gbc.gridwidth = GridBagConstraints.REMAINDER;     
         gb.setConstraints(piApprox, gbc);
         messageArea.add(piApprox);
-        gbc.gridwidth = GridBagConstraints.RELATIVE;
+        
+        gbc.gridwidth = GridBagConstraints.RELATIVE;         
         gb.setConstraints(countLabel, gbc);
         messageArea.add(countLabel);
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
+
+        gbc.gridwidth = GridBagConstraints.REMAINDER;        
         gb.setConstraints(piLabel, gbc);
         messageArea.add(piLabel);
+        
+        gbc2.gridwidth = GridBagConstraints.REMAINDER;
+        gb2.setConstraints(inicioLabel, gbc2);
+        messageArea.add(inicioLabel);
+        
         return messageArea;
     }
 
@@ -86,6 +117,9 @@ public class MonteCarloApplet extends Applet implements Runnable, ActionListener
 
     // Llamado desde Thread.start (); parte de la interfaz Runnable.
     public void run() {
+                hora();
+                inicioLabel.setText(horaStr);
+
         while (Thread.currentThread() == monteCarloThread) {
             if (restart_p) {
                 restart_p = false;
@@ -138,6 +172,16 @@ public class MonteCarloApplet extends Applet implements Runnable, ActionListener
         } else if (cmd.equals("Reiniciar")) {
             restart_p = true;
         }
-
     }
+
+    private void hora() {
+        Calendar calendario = new GregorianCalendar();
+        int hora, minutos, segundos;
+        hora = calendario.get(Calendar.HOUR_OF_DAY);
+        minutos = calendario.get(Calendar.MINUTE);
+        segundos = calendario.get(Calendar.SECOND);
+        horaStr = String.valueOf(hora) + ":" + String.valueOf(minutos) + ":" + String.valueOf(segundos);
+        System.out.println(horaStr);
+    }
+    
 }
